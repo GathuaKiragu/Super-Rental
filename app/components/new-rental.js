1386898513a3d7d84144d9594296caecc85e4a62
Gathua Kiragu <kiragugathua@gmail.com>
2017-03-30 12:41:18 +0300

    Added a review section This section will enable users to update reviews on rentals

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -1,20 +1,20 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
-addNewRental: false
-, actions: {
+  addNewRental: false,
+  actions: {
     rentalFormShow() {
-            this.set('addNewRental', true);
-        }
-        , saveRental1() {
-            var params = {
-                owner: this.get('owner'),
-                 city: this.get('city'), 
-                 type: this.get('type'),
-                 image: this.get('image'),
-                 bedrooms: this.get('bedrooms'),
-                   };
-            this.set('addNewRental', false);
-            this.sendAction('saveRental2', params);
-        }
-}
+      this.set('addNewRental', true);
+    },
+    saveRental1() {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.set('addNewRental', false);
+      this.sendAction('saveRental2', params);
+    }
+  }
 });
--- app/models/review.js
+++ app/models/review.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   ratiing: DS.attr(),
-  content: DS.attr()
+  content: DS.attr(),
+  rentals: DS.belongsTo('rental', {async: true})
 });
